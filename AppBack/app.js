56242d7ff06f43f101701191a9b7709198cbd32a
aznaranivan <145350718+aznaranivan@users.noreply.github.com>
2025-10-15 18:30:12 +0200

    Actualización: backend listo y frontend compilado

--- AppBack/app.js
+++ AppBack/app.js
@@ -1,22 +1,31 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); // 🔹 nuevo
 
 require('dotenv').config();
-require('./config/db')
+require('./config/db');
 
 const app = express();
 
 // Config
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// GET /api/usuarios
-// GET /api/usuarios/USUARIOID
+// Rutas API
 app.use('/api', require('./routes/api'));
 
+// Servir los archivos estáticos desde la carpeta 'public/browser'
+app.use(express.static(path.join(__dirname, 'public', 'browser')));
+
+// Redirigir todas las rutas al index.html de Angular
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'browser', 'index.html'));
+});
+
+
+// Puerto
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en puerto ${PORT}`);
-})
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+});
