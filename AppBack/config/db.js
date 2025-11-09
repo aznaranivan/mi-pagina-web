214121c4443808e9df79075372b48e90f9cfc2cf
Ivan-Aznaran <ivanarturo.aznaran@alumno.ucjc.edu>
2025-11-09 18:40:53 +0100

    Mejor manejo de errores en /api/contacto

--- AppBack/config/db.js
+++ AppBack/config/db.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ Conectado a MongoDB Atlas"))
+  .catch(err => console.error("❌ Error al conectar con MongoDB:", err));
